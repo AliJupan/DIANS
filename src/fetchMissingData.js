4e291d4e008b9f2299f3073241a37cf38c42bdd6
Ali Jupani <alizhupani2002@gmail.com>
2024-11-08 23:35:11 +0100

    minor fix

--- src/fetchMissingData.js
+++ src/fetchMissingData.js
@@ -1,22 +1,19 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 import moment from "moment";
-import insertDataToDb from "./insertDataToDb.js"; 
+import insertDataToDb from "./insertDataToDb.js";
 
 async function fetchMissingData(issuerCode, startDate) {
   const url = "https://www.mse.mk/mk/stats/symbolhistory/REPL";
 
-  // let currentStartDate = startDate
-  //   ? moment(startDate)
-  //   : moment().subtract(10, "years");
+  let currentStartDate = startDate
+    ? moment(startDate)
+    : moment().subtract(10, "years");
 
-  while (startDate.isBefore(moment())) {
-    const currentEndDate = startDate
-      .clone()
-      .add(1, "year")
-      .subtract(1, "day");
+  while (currentStartDate.isBefore(moment())) {
+    const currentEndDate = currentStartDate.clone().add(1, "year").subtract(1, "day");
 
-    const formattedStartDate = startDate.format("DD.MM.YYYY");
+    const formattedStartDate = currentStartDate.format("DD.MM.YYYY");
     const formattedEndDate = currentEndDate.isAfter(moment())
       ? moment().format("DD.MM.YYYY")
       : currentEndDate.format("DD.MM.YYYY");
@@ -70,7 +67,7 @@ async function fetchMissingData(issuerCode, startDate) {
         } = transaction;
         if (lastTransactionPrice != null) {
           const parsedDate = new Date(parseDate(date));
-          const parsedQuantity = quantity ? parseInt(quantity) :  0;
+          const parsedQuantity = quantity ? parseInt(quantity) : 0;
 
           await insertDataToDb(
             issuerCode,
@@ -87,7 +84,7 @@ async function fetchMissingData(issuerCode, startDate) {
         }
       }
 
-      startDate = currentEndDate.clone().add(1, "day");
+      currentStartDate = currentEndDate.clone().add(1, "day");
     } catch (error) {
       console.error(`Error fetching missing data for ${issuerCode}:`, error);
       break;
