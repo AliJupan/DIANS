8f2eb4f235b0372d452ab81ff909e27c0d446ba4
Ali Jupani <alizhupani2002@gmail.com>
2025-01-18 22:17:34 +0100

    added vercel configuration file for backend deploying and dockerCompose file

--- Homework4/BackEnd/src/index.js
+++ Homework4/BackEnd/src/index.js
@@ -22,7 +22,7 @@ connectDB();
 
 app.use('/stock',stockPriceRouter(stockPriceController));
 
-// populateDatabase();
+populateDatabase();
 
 app.listen(5400, () => {
   console.log(`Server is running on port ${5400}`);
